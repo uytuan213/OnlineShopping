6d13675cd70524dee808ad5f46c9bbb230377bca
unknown <uytuan213@gmail.com>
2019-03-08 13:43:00 -0500

    change root password

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php 
 	$servername = "localhost";
 	$username = "root";
-	$password = "";
+	$password = "root";
 	$db = "onlineshopping";
 
 	$con = mysqli_connect($servername, $username, $password, $db);
